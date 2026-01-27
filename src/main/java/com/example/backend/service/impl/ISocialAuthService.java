0f5b86952b4d21273e36519156939947685a2b08
huyvu0607 <vudh1092@ut.edu.vn>
2026-01-27 23:23:40 +0700

    delete file

--- src/main/java/com/example/backend/service/impl/ISocialAuthService.java
+++
@@ -1,8 +0,0 @@
-package com.example.backend.service;
-
-import com.example.backend.dto.auth.SocialUserInfo;
-
-public interface ISocialAuthService {
-    SocialUserInfo verifyGoogleToken(String accessToken);
-    SocialUserInfo verifyGithubToken(String accessToken);
-}
--- src/main/java/com/example/backend/service/impl/IUserLockService.java
+++
@@ -1,6 +0,0 @@
-package com.example.backend.service.impl;
-
-public interface IUserLockService {
-    void handleFailedLogin(Long userID);
-    void resetFailedAttempts(Long userId);
-}
